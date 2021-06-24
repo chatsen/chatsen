fb188f4b1fef534cb5cc8716895c9f9a7b47cc84
Anonymous <Anonymous>
2021-06-24 17:55:41 +0200

    fix: ios login for new oauth page

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,7 +6,7 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:device_info_plus_web/device_info_plus_web.dart';
-import 'package:file_picker/src/file_picker_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:file_selector_web/file_selector_web.dart';
 import 'package:package_info_plus_web/package_info_plus_web.dart';
 import 'package:share_plus_web/share_plus_web.dart';
