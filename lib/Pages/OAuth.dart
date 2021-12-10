b25c96e4d3d68f78c18751293b158f32329e8cf3
williambulin <williambulin01@gmail.com>
2021-12-10 18:28:33 +0100

    change: removed unused/unusable oauth permissions

--- lib/Pages/OAuth.dart
+++ lib/Pages/OAuth.dart
@@ -121,9 +121,45 @@ class _OAuthPageState extends State<OAuthPage> {
               Navigator.of(context).pop();
             }
           },
-          initialUrl: 'https://id.twitch.tv/oauth2/authorize?client_id=vvxbprk8sfufgzd7k9wwr1478znf7b&redirect_uri=https://chatsen.app/oauth&response_type=token&scope=user_subscriptions%20user_blocks_edit%20user_blocks_read%20user_follows_edit%20channel_editor%20channel:moderate%20channel:read:redemptions%20chat:edit%20chat:read%20whispers:read%20whispers:edit%20channel_commercial%20channel:edit:commercial%20user:edit:follows%20clips:edit%20channel:manage:broadcast%20user:read:blocked_users%20user:manage:blocked_users%20moderator:manage:automod',
+          initialUrl: 'https://id.twitch.tv/oauth2/authorize?client_id=vvxbprk8sfufgzd7k9wwr1478znf7b&redirect_uri=https://chatsen.app/oauth&response_type=token&scope=' +
+              [
+                // Chat
+                'chat:edit',
+                'chat:read',
+                'channel:moderate',
+                'channel:read:redemptions',
+                'moderator:manage:automod',
+                'channel:manage:broadcast',
+
+                // Whispers
+                'whispers:read',
+                'whispers:edit',
+
+                // Sub emotes
+                'user_subscriptions',
+
+                // Blocked users
+                'user_blocks_edit',
+                'user_blocks_read',
+                'user:read:blocked_users',
+                'user:manage:blocked_users',
+
+                // Raid
+                'channel_editor', // Raids
+
+                // Run ads
+                // 'channel_commercial',
+                // 'channel:edit:commercial',
+
+                // To create clips
+                // 'clips:edit',
+
+                // Follows, no longer working
+                // 'user_follows_edit',
+                // 'user:edit:follows',
+              ].join('%20'),
           javascriptMode: JavascriptMode.unrestricted,
-          userAgent: 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.4) Gecko/20100101 Firefox/4.0',
+          userAgent: 'Chatsen',
         ),
       );
 }
