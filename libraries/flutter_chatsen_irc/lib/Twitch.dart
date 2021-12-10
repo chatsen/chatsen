e4375c959c6afc94a2fc395560e6e7ec8f499eb1
williambulin <williambulin01@gmail.com>
2021-12-10 18:29:34 +0100

    fix: handle recent-messages malformed IRC messages

--- libraries/flutter_chatsen_irc/lib/Twitch.dart
+++ libraries/flutter_chatsen_irc/lib/Twitch.dart
@@ -358,11 +358,19 @@ class Channel {
         continue;
       }
 
+      if (message!.parameters.length > 2) {
+        print(message.parameters[1]);
+        message.parameters = [
+          message.parameters[0],
+          message.parameters.sublist(1).join(':'),
+        ];
+      }
+
       var chatMessage = Message(
         channel: this,
         history: true,
         user: User(
-          login: message!.prefix.split('!').first.toLowerCase(),
+          login: message.prefix.split('!').first.toLowerCase(),
           displayName: message.tags['display-name'],
           id: int.tryParse(message.tags['user-id'] ?? '0'),
           color: (message.tags['color'] == null || message.tags['color'].trim().isEmpty ? null : message.tags['color'].substring(1)),
