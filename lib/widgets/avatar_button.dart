b4586b11e5407f201eec0896fc9fe6104a25cbcf
williambulin <williambulin01@gmail.com>
2023-05-04 13:13:14 +0200

    Reworked avatar button and added chat search

--- lib/widgets/avatar_button.dart
+++ lib/widgets/avatar_button.dart
@@ -31,103 +31,100 @@ class _AvatarButtonState extends State<AvatarButton> {
       bloc: BlocProvider.of<NotificationsCubit>(context),
       builder: (BuildContext context, state) => Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(3.0),
-            child: Material(
+          Material(
+            borderRadius: BorderRadius.circular(128.0),
+            color: Theme.of(context).colorScheme.surfaceVariant,
+            child: InkWell(
+              onTap: () => Modal.show(
+                context: context,
+                child: const NotificationsModal(),
+              ),
               borderRadius: BorderRadius.circular(128.0),
-              color: Theme.of(context).colorScheme.surfaceVariant,
-              child: InkWell(
-                onTap: () => Modal.show(
-                  context: context,
-                  child: const NotificationsModal(),
-                ),
-                borderRadius: BorderRadius.circular(128.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const SizedBox(width: 6.0),
-                    const Padding(
-                      padding: EdgeInsets.all(4.0),
-                      child: Icon(Icons.notifications_none_outlined),
-                    ),
-                    Surface(
-                      borderRadius: BorderRadius.circular(128.0),
-                      type: SurfaceType.background,
-                      // color: Theme.of(context).colorScheme.tertiaryContainer,
-                      child: BoxListener(
-                        box: Hive.box('AccountSettings'),
-                        builder: (context, accountSettingsBox) {
-                          return BoxListener(
-                            box: Hive.box('TwitchAccounts'),
-                            builder: (BuildContext context, Box twitchAccountsBox) {
-                              final twitchAccount = twitchAccountsBox.values.firstWhere((element) => accountSettingsBox.get('activeTwitchAccount') == element.tokenData.hash, orElse: () => null);
-                              return Material(
-                                color: Colors.transparent,
-                                borderRadius: BorderRadius.circular(24.0),
-                                clipBehavior: Clip.antiAlias,
-                                child: GestureDetector(
-                                  onPanStart: (details) {
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(width: 6.0),
+                  const Padding(
+                    padding: EdgeInsets.all(4.0),
+                    child: Icon(Icons.notifications_none_outlined),
+                  ),
+                  Surface(
+                    borderRadius: BorderRadius.circular(128.0),
+                    type: SurfaceType.background,
+                    // color: Theme.of(context).colorScheme.tertiaryContainer,
+                    child: BoxListener(
+                      box: Hive.box('AccountSettings'),
+                      builder: (context, accountSettingsBox) {
+                        return BoxListener(
+                          box: Hive.box('TwitchAccounts'),
+                          builder: (BuildContext context, Box twitchAccountsBox) {
+                            final twitchAccount = twitchAccountsBox.values.firstWhere((element) => accountSettingsBox.get('activeTwitchAccount') == element.tokenData.hash, orElse: () => null);
+                            return Material(
+                              color: Colors.transparent,
+                              borderRadius: BorderRadius.circular(24.0),
+                              clipBehavior: Clip.antiAlias,
+                              child: GestureDetector(
+                                onPanStart: (details) {
+                                  direction = AvatarButtonDirection.none;
+                                },
+                                onPanUpdate: (details) {
+                                  if (details.delta.dy > 1.0) {
+                                    direction = AvatarButtonDirection.down;
+                                  } else if (details.delta.dy < -1.0) {
+                                    direction = AvatarButtonDirection.up;
+                                  } else {
                                     direction = AvatarButtonDirection.none;
+                                  }
+                                },
+                                onPanEnd: (details) {
+                                  var activeAccountHash = accountSettingsBox.get('activeTwitchAccount');
+                                  var indexOfActiveAccountHash = twitchAccountsBox.keys.toList().indexOf(activeAccountHash);
+                                  switch (direction) {
+                                    case AvatarButtonDirection.up:
+                                      ++indexOfActiveAccountHash;
+                                      if (indexOfActiveAccountHash >= twitchAccountsBox.length) indexOfActiveAccountHash = 0;
+                                      break;
+                                    case AvatarButtonDirection.down:
+                                      --indexOfActiveAccountHash;
+                                      if (indexOfActiveAccountHash < 0) indexOfActiveAccountHash = twitchAccountsBox.keys.toList().length - 1;
+                                      break;
+                                    case AvatarButtonDirection.none:
+                                      break;
+                                  }
+                                  accountSettingsBox.put('activeTwitchAccount', twitchAccountsBox.keys.elementAt(indexOfActiveAccountHash));
+                                  direction = AvatarButtonDirection.none;
+                                },
+                                child: InkWell(
+                                  onTap: () async {
+                                    Modal.show(
+                                      context: context,
+                                      child: const ChatsenModal(),
+                                    );
                                   },
-                                  onPanUpdate: (details) {
-                                    if (details.delta.dy > 1.0) {
-                                      direction = AvatarButtonDirection.down;
-                                    } else if (details.delta.dy < -1.0) {
-                                      direction = AvatarButtonDirection.up;
-                                    } else {
-                                      direction = AvatarButtonDirection.none;
-                                    }
-                                  },
-                                  onPanEnd: (details) {
-                                    var activeAccountHash = accountSettingsBox.get('activeTwitchAccount');
-                                    var indexOfActiveAccountHash = twitchAccountsBox.keys.toList().indexOf(activeAccountHash);
-                                    switch (direction) {
-                                      case AvatarButtonDirection.up:
-                                        ++indexOfActiveAccountHash;
-                                        if (indexOfActiveAccountHash >= twitchAccountsBox.length) indexOfActiveAccountHash = 0;
-                                        break;
-                                      case AvatarButtonDirection.down:
-                                        --indexOfActiveAccountHash;
-                                        if (indexOfActiveAccountHash < 0) indexOfActiveAccountHash = twitchAccountsBox.keys.toList().length - 1;
-                                        break;
-                                      case AvatarButtonDirection.none:
-                                        break;
-                                    }
-                                    accountSettingsBox.put('activeTwitchAccount', twitchAccountsBox.keys.elementAt(indexOfActiveAccountHash));
-                                    direction = AvatarButtonDirection.none;
-                                  },
-                                  child: InkWell(
-                                    onTap: () async {
-                                      Modal.show(
-                                        context: context,
-                                        child: const ChatsenModal(),
-                                      );
-                                    },
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Material(
-                                        borderRadius: BorderRadius.circular(24.0),
-                                        clipBehavior: Clip.antiAlias,
-                                        child: SizedBox(
-                                          width: 32.0,
-                                          height: 32.0,
-                                          child: Ink.image(
-                                            image: NetworkImage(twitchAccount.userData.avatarUrl),
-                                            fit: BoxFit.cover,
-                                          ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Material(
+                                      borderRadius: BorderRadius.circular(24.0),
+                                      clipBehavior: Clip.antiAlias,
+                                      child: SizedBox(
+                                        width: 32.0,
+                                        height: 32.0,
+                                        child: Ink.image(
+                                          image: NetworkImage(twitchAccount.userData.avatarUrl),
+                                          fit: BoxFit.cover,
                                         ),
                                       ),
                                     ),
                                   ),
                                 ),
-                              );
-                            },
-                          );
-                        },
-                      ),
+                              ),
+                            );
+                          },
+                        );
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
--- lib/widgets/chat_view.dart
+++ lib/widgets/chat_view.dart
@@ -1,9 +1,15 @@
+import 'dart:math';
+
+import 'package:chatsen/tmi/channel/messages/channel_message_chat.dart';
+import 'package:chatsen/widgets/avatar_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../components/boxlistener.dart';
+import '../components/surface.dart';
+import '../data/notifications_cubit.dart';
 import '../tmi/channel/channel_message.dart';
 import 'chat_message.dart';
 import '/tmi/channel/channel.dart';
@@ -26,6 +32,7 @@ class ChatView extends StatefulWidget {
 }
 
 class _ChatViewState extends State<ChatView> {
+  TextEditingController searchController = TextEditingController();
   ScrollController scrollController = ScrollController();
   List<ChannelMessage>? scrollMessages;
 
@@ -36,6 +43,7 @@ class _ChatViewState extends State<ChatView> {
 
   @override
   void dispose() {
+    searchController.dispose();
     scrollController.dispose();
     super.dispose();
   }
@@ -48,9 +56,10 @@ class _ChatViewState extends State<ChatView> {
           bloc: widget.channel.channelMessages,
           buildWhen: (state1, state2) => scrollMessages == null,
           builder: (context, state) {
-            var messages = (scrollMessages ?? state);
+            var messages = (scrollMessages ?? state).reversed;
+            if (scrollMessages != null && searchController.text.isNotEmpty) messages = messages.where((element) => (element is ChannelMessageChat && '${(element.user.displayName ?? element.user.login)?.toLowerCase()}: ${element.body.toLowerCase()}'.contains(searchController.text.toLowerCase()))).toList();
             if (widget.filter != null) messages = messages.where((element) => widget.filter!(element)).toList();
-            return Column(
+            return Stack(
               children: [
                 Expanded(
                   child: NotificationListener<ScrollNotification>(
@@ -71,49 +80,119 @@ class _ChatViewState extends State<ChatView> {
                       reverse: true,
                       itemCount: messages.length,
                       controller: scrollController,
-                      itemBuilder: (BuildContext context, int index) => ChatMessage(
-                        key: ObjectKey(messages.reversed.elementAt(index)),
-                        message: messages.reversed.elementAt(index),
-                      ),
+                      itemBuilder: (BuildContext context, int index) {
+                        return ChatMessage(
+                          key: ObjectKey(messages.elementAt(index)),
+                          message: messages.elementAt(index),
+                        );
+                      },
                     ),
                   ),
                 ),
-                Material(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
+                // Material(
+                //   color: Theme.of(context).colorScheme.primaryContainer,
+                //   child: Column(
+                //     mainAxisSize: MainAxisSize.min,
+                //     children: [
+                //       if (scrollMessages != null)
+                //         Ink(
+                //           color: Theme.of(context).colorScheme.tertiaryContainer,
+                //           height: 32.0 + 24.0,
+                //           child: InkWell(
+                //             onTap: () {
+                //               scrollMessages = null;
+                //               setState(() {});
+                //               WidgetsBinding.instance.scheduleFrameCallback((_) {
+                //                 scrollController.jumpTo(scrollController.position.minScrollExtent);
+                //               });
+                //             },
+                //             child: Row(
+                //               mainAxisAlignment: MainAxisAlignment.center,
+                //               children: [
+                //                 Icon(
+                //                   Icons.arrow_downward_rounded,
+                //                   color: Theme.of(context).colorScheme.onTertiaryContainer,
+                //                 ),
+                //                 const SizedBox(width: 4.0),
+                //                 Text(
+                //                   AppLocalizations.of(context)!.scrollToBottom,
+                //                   style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                //                         color: Theme.of(context).colorScheme.onTertiaryContainer,
+                //                       ),
+                //                 ),
+                //               ],
+                //             ),
+                //           ),
+                //         ),
+                //     ],
+                //   ),
+                // ),
+                SafeArea(
+                  child: Row(
                     children: [
-                      if (scrollMessages != null)
-                        Ink(
-                          color: Theme.of(context).colorScheme.tertiaryContainer,
-                          height: 32.0 + 24.0,
-                          child: InkWell(
-                            onTap: () {
-                              scrollMessages = null;
-                              setState(() {});
-                              WidgetsBinding.instance.scheduleFrameCallback((_) {
-                                scrollController.jumpTo(scrollController.position.minScrollExtent);
-                              });
-                            },
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.arrow_downward_rounded,
-                                  color: Theme.of(context).colorScheme.onTertiaryContainer,
+                      Expanded(
+                        child: AnimatedOpacity(
+                          duration: const Duration(milliseconds: 200),
+                          opacity: scrollMessages != null ? 1.0 : 0.0,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                            child: Surface(
+                              borderRadius: BorderRadius.circular(128.0),
+                              // color: Colors.white,
+                              type: SurfaceType.surfaceVariant,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                                child: TextField(
+                                  controller: searchController,
+                                  decoration: const InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'Search',
+                                    isDense: true,
+                                  ),
+                                  onChanged: (text) => setState(() {}),
                                 ),
-                                const SizedBox(width: 4.0),
-                                Text(
-                                  AppLocalizations.of(context)!.scrollToBottom,
-                                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                                        color: Theme.of(context).colorScheme.onTertiaryContainer,
-                                      ),
-                                ),
-                              ],
+                              ),
                             ),
                           ),
                         ),
+                      ),
+                      if (widget.filter == null)
+                        AnimatedOpacity(
+                          duration: const Duration(milliseconds: 200),
+                          opacity: (scrollMessages != null || BlocProvider.of<NotificationsCubit>(context).state.unread > 0) ? 1.0 : 0.0,
+                          child: const Padding(
+                            padding: EdgeInsets.only(top: 8.0, right: 8.0, bottom: 8.0),
+                            child: AvatarButton(),
+                          ),
+                        ),
                     ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 24.0,
+                  right: 24.0,
+                  child: AnimatedOpacity(
+                    duration: const Duration(milliseconds: 200),
+                    opacity: scrollMessages != null ? 1.0 : 0.0,
+                    child: Surface(
+                      onTap: () {
+                        scrollMessages = null;
+                        setState(() {});
+                        WidgetsBinding.instance.scheduleFrameCallback((_) {
+                          scrollController.jumpTo(scrollController.position.minScrollExtent);
+                        });
+                        searchController.text = '';
+                      },
+                      borderRadius: BorderRadius.circular(128.0),
+                      type: SurfaceType.tertiary,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Transform.rotate(
+                          angle: 90 * pi / 180,
+                          child: const Icon(Icons.chevron_right_outlined),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
