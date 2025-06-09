d4a8c9109ac02ec14597a1362ed0d3de19f9df9b
chatsen <chatsen>
2025-06-09 04:04:19 +0200

    Removed user agent requirement on iOS

--- lib/Pages/OAuth.dart
+++ lib/Pages/OAuth.dart
@@ -159,7 +159,7 @@ class _OAuthPageState extends State<OAuthPage> {
                 // 'user:edit:follows',
               ].join('%20'),
           javascriptMode: JavascriptMode.unrestricted,
-          userAgent: Platform.isIOS ? 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' : null,
+          // userAgent: Platform.isIOS ? 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36' : null,
         ),
       );
 }
